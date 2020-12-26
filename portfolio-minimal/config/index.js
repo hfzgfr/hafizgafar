42ea42a641a41607c0b841390a84095cc953cfbd
hfzgfr <hfzgfr@gmail.com>
2020-12-26 19:56:06 +0800

    added info

--- portfolio-minimal/config/index.js
+++ portfolio-minimal/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
-  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  seoTitleSuffix: "Hafiz Gafar Portfolio", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
   // -- THEME SETTINGS -----
   colors: {
@@ -84,10 +84,6 @@ module.exports = {
     {
       name: "Privacy",
       url: "/privacy",
-    },
-    {
-      name: "Imprint",
-      url: "/imprint",
     },
   ],
 }
